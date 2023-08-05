3df78fc5b2dba1882a72e97f6eda959ff98d7187
Mountacir <111779985+M0untacir@users.noreply.github.com>
2023-08-05 13:15:30 +0100

    Update docker-build.sh

--- rocm-gfx803-pytorch-builder/docker-build.sh
+++ rocm-gfx803-pytorch-builder/docker-build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 source ../build-args.sh
 
-docker_image_name="ulyssesrr/rocm-gfx803-pytorch-builder"
+docker_image_name="reda2020/rocm-gfx803-pytorch-builder"
 docker_image_tag="ubuntu${ARG_UBUNTU_VERSION}-rocm${ARG_ROCM_VERSION}-complete"
 
 echo "Building $docker_image_name:$docker_image_tag ..."
 docker_build -t "$docker_image_name:$docker_image_tag" .
+
+
+docker push "$docker_image_name:$docker_image_tag"
+docker tag "$docker_image_name:$docker_image_tag" "$docker_image_name:latest"
+docker push "$docker_image_name:latest"
