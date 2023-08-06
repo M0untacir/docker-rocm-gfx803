dd7fddafac7594b5cf4213509ee20335c627aec9
Mountacir <111779985+M0untacir@users.noreply.github.com>
2023-08-06 10:48:32 +0100

    Update docker-build.sh

--- rocm-gfx803-pytorch-builder/docker-build.sh
+++ rocm-gfx803-pytorch-builder/docker-build.sh
@@ -7,11 +7,4 @@ docker_image_tag="ubuntu${ARG_UBUNTU_VERSION}-rocm${ARG_ROCM_VERSION}-complete"
 echo "Building $docker_image_name:$docker_image_tag ..."
 docker_build -t "$docker_image_name:$docker_image_tag" .
 
-docker create --name dummy "$docker_image_name:$docker_image_tag"
-docker cp dummy:/current/build /home/
-docker rm -f dummy
 
-
-docker push "$docker_image_name:$docker_image_tag"
-docker tag "$docker_image_name:$docker_image_tag" "$docker_image_name:latest"
-docker push "$docker_image_name:latest"
