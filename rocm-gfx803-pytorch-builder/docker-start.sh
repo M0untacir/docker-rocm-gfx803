944783365aef618308b101febc0a84b8d1998e1e
Mountacir <111779985+M0untacir@users.noreply.github.com>
2023-08-06 08:43:39 +0100

    Update docker-start.sh

--- rocm-gfx803-pytorch-builder/docker-start.sh
+++ rocm-gfx803-pytorch-builder/docker-start.sh
@@ -9,3 +9,13 @@ docker_image_name="ulyssesrr/rocm-gfx803-pytorch-builder"
 docker_image_tag="ubuntu${ARG_UBUNTU_VERSION}-rocm${ARG_ROCM_VERSION}-complete"
 
 drun --rm "$docker_image_name:$docker_image_tag" "$@"
+
+
+# docker create --name dummy "$docker_image_name:$docker_image_tag"
+# docker cp dummy:/current /home
+# docker rm -f dummy
+
+
+# docker push "$docker_image_name:$docker_image_tag"
+# docker tag "$docker_image_name:$docker_image_tag" "$docker_image_name:latest"
+# docker push "$docker_image_name:latest"
