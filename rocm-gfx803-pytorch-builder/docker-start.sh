1e2dd8548d197a93da6b2072ca1fa5636da56531
Mountacir <111779985+M0untacir@users.noreply.github.com>
2023-08-07 15:21:41 +0100

    Update docker-start.sh

--- rocm-gfx803-pytorch-builder/docker-start.sh
+++ rocm-gfx803-pytorch-builder/docker-start.sh
@@ -2,7 +2,7 @@
 source ../build-args.sh
 
 drun() {
-   sudo docker run -t --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v $(pwd):/current "$@"
+   sudo docker run -t --privileged --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v $(pwd):/current "$@"
 }
 
 docker_image_name="reda2020/rocm-gfx803-pytorch-builder"
@@ -10,12 +10,3 @@ docker_image_tag="ubuntu${ARG_UBUNTU_VERSION}-rocm${ARG_ROCM_VERSION}-complete"
 
 drun --rm "$docker_image_name:$docker_image_tag" "$@"
 
-
-# docker create --name dummy "$docker_image_name:$docker_image_tag"
-# docker cp dummy:/current /home
-# docker rm -f dummy
-
-
-# docker push "$docker_image_name:$docker_image_tag"
-# docker tag "$docker_image_name:$docker_image_tag" "$docker_image_name:latest"
-# docker push "$docker_image_name:latest"
